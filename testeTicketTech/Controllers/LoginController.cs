e394a47493919b6fdafb0b0e650e123a7fa2d606
LojuanBryan <lojuanbryan9@gmail.com>
2025-10-21 17:40:55 -0300

    Validação do check-box

--- testeTicketTech/Controllers/LoginController.cs
+++ testeTicketTech/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using testeTicketTech.Models;
 
 namespace testeTicketTech.Controllers
 {
@@ -8,5 +9,35 @@ namespace testeTicketTech.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Entrar(LoginModel loginModel)
+        {
+            try
+            {
+                // Valida login e senha manualmente
+                if (string.IsNullOrEmpty(loginModel.Login) || string.IsNullOrEmpty(loginModel.Senha))
+                {
+                    ModelState.AddModelError(string.Empty, "Preencha o login e a senha.");
+                    return View("Index", loginModel);
+                }
+
+                // Valida checkbox manualmente
+                if (!loginModel.ConcordaLGPD)
+                {
+                    ModelState.AddModelError("ConcordaLGPD", "Você deve concordar com a LGPD para continuar.");
+                    return View("Index", loginModel);
+                }
+
+                // Se tudo estiver ok, redireciona
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops! Não conseguimos realizar o seu login. Detalhe: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+
     }
 }
---
+++ testeTicketTech/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testeTicketTech.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Digite o Login.")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Digite a senha.")]
+        public string Senha { get; set; }
+        public bool ConcordaLGPD { get; set; }
+    }
+}
+
