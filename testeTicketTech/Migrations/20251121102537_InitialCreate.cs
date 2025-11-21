e0871c3a37679f7f8aff8da7636b774e0dd9740b
Pedro Marques <pedrosdmh@gmail.com>
2025-11-21 07:29:16 -0300

    mensagem do commit

---
+++ testeTicketTech/Migrations/20251121102537_InitialCreate.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace testeTicketTech.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TokenRedefinicao",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "Chamados",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chamados_UsuarioId",
+                table: "Chamados",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chamados_Usuarios_UsuarioId",
+                table: "Chamados",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chamados_Usuarios_UsuarioId",
+                table: "Chamados");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Chamados_UsuarioId",
+                table: "Chamados");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TokenRedefinicao",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "Chamados",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
